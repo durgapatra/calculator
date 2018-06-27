29aa330caa0c0548bac6a93450ee908cc2c402db
durga <durgaprasadpatra.in@gmail.com>
2018-06-27 19:59:54 +0530

    added keyBoard event

--- src/ResultScreen.jsx
+++ src/ResultScreen.jsx
@@ -14,15 +14,26 @@ export default class Calculator extends React.Component {
             oldValue: 0,
             selectSymbol: ""
         }
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+
+        this.handleMultiplication = this.handleMultiplication.bind(this);
+        this.handleDivision = this.handleDivision.bind(this);
+        this.handleAddition = this.handleAddition.bind(this);
+        this.handleSubtraction = this.handleSubtraction.bind(this);
+
+        this.getTotale = this.getTotale.bind(this);
     }
 
     onHandlePress(value) {
-        var displayvalue = (this.state.displayvalue === "0" ? "" : this.state.displayvalue) + value;
+        var displayvalue = (this.state.displayvalue === "0" ? "" : (this.state.displayvalue).toString()) + value;
         this.setState({ displayvalue })
     }
 
     onClear() {
-        this.setState({ displayvalue: "0" })
+        this.setState({
+            displayvalue: "0", oldValue: 0,
+            selectSymbol: ""
+        })
     }
 
     handleDivision() {
@@ -60,8 +71,6 @@ export default class Calculator extends React.Component {
         })
     }
     getTotale() {
-        console.log(this.state.selectSymbol);
-
         switch (this.state.selectSymbol) {
             case "/":
                 let divisionTotal = parseInt(this.state.oldValue, 10) / parseInt(this.state.displayvalue, 10);
@@ -84,6 +93,50 @@ export default class Calculator extends React.Component {
                 break;
         }
     }
+
+    handleKeyPress = (event) => {
+
+        let { key } = event;
+        if (key === 'Backspace') {
+            event.preventDefault()
+            this.onClear()
+        }
+        if (key === '*') {
+            event.preventDefault()
+            this.handleMultiplication()
+        }
+        if (key === '/') {
+            event.preventDefault()
+            this.handleDivision()
+        }
+        if (key === '+') {
+            event.preventDefault()
+            this.handleAddition()
+        }
+        if (key === '-') {
+            event.preventDefault()
+            this.handleSubtraction()
+        }
+        if (parseInt(key, 10)) {
+            event.preventDefault()
+            this.onHandlePress.bind(this, key)()
+
+        }
+        if (key === 'Enter') {
+            event.preventDefault()
+            this.getTotale();
+        }
+
+    }
+
+    componentDidMount() {
+        // window.addEventListener(KeyboardEvent)
+        window.addEventListener('keydown', this.handleKeyPress)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('keydown', this.handleKeyDown)
+    }
     render() {
         return (
             <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
@@ -94,24 +147,24 @@ export default class Calculator extends React.Component {
                     <div style={{ overflow: "hidden", display: "flex", flexDirection: "row" }}>
                         <div >
                             <MyButton onClick={this.onClear.bind(this)} className="btn btn-light" style={{ width: "240px", height: "50px" }} titale="Clear" />
-                            <Division onClick={this.handleDivision.bind(this)} />
+                            <Division onClick={this.handleDivision} />
 
                             <MyButton onClick={this.onHandlePress.bind(this, 7)} className="btn btn-light" style={{ width: "80px", height: "50px" }} titale="7" />
                             <MyButton onClick={this.onHandlePress.bind(this, 8)} className="btn btn-light" style={{ width: "80px", height: "50px" }} titale="8" />
                             <MyButton onClick={this.onHandlePress.bind(this, 9)} className="btn btn-light" style={{ width: "80px", height: "50px" }} titale="9" />
-                            <Subtraction onClick={this.handleSubtraction.bind(this)} />
+                            <Subtraction onClick={this.handleSubtraction} />
 
                             <MyButton onClick={this.onHandlePress.bind(this, 4)} className="btn btn-light" style={{ width: "80px", height: "50px" }} titale="4" />
                             <MyButton onClick={this.onHandlePress.bind(this, 5)} className="btn btn-light" style={{ width: "80px", height: "50px" }} titale="5" />
                             <MyButton onClick={this.onHandlePress.bind(this, 6)} className="btn btn-light" style={{ width: "80px", height: "50px" }} titale="6" />
-                            <Addition onClick={this.handleAddition.bind(this)} />
+                            <Addition onClick={this.handleAddition} />
 
                             <MyButton onClick={this.onHandlePress.bind(this, 1)} className="btn btn-light" style={{ width: "80px", height: "50px" }} titale="1" />
                             <MyButton onClick={this.onHandlePress.bind(this, 2)} className="btn btn-light" style={{ width: "80px", height: "50px" }} titale="2" />
                             <MyButton onClick={this.onHandlePress.bind(this, 3)} className="btn btn-light" style={{ width: "80px", height: "50px" }} titale="3" />
-                            <Multiplication onClick={this.handleMultiplication.bind(this)} />
+                            <Multiplication onClick={this.handleMultiplication} />
 
-                            <button onClick={this.getTotale.bind(this)} className="btn  btn-block" style={{ height: "50px", backgroundColor: "#f2274f" }} > =</button>
+                            <button onClick={this.getTotale} className="btn  btn-block" style={{ height: "50px", backgroundColor: "#f2274f" }} > =</button>
                         </div>
 
                     </div>
